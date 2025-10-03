1e8970c866bfc3a0dd066d70003d6e92a96d6d60
Manh Quyet <c3lttrong.0a9.ntmquyet@gmail.com>
2025-10-03 08:20:16 +0700

    thay doi file

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ const port = 3000
 
 app.use(express.static(path.join(__dirname,'public')))
 
+app.use(express.urlencoded({
+  extened: true
+}))
+app.use(express.json())
+
 app.use(morgan('combined'))
 
 app.engine('hbs', engine({
@@ -25,8 +30,14 @@ app.get('/news', (req, res) => {
 })
 
 app.get('/search', (req, res) => {
-  console.log(req.query.q)
   res.render('search')
+})
+
+app.post('/search',(req, res) => {
+
+  console.log(req.body)
+
+  res.send('')
 })
 
 app.listen(port, () => {
